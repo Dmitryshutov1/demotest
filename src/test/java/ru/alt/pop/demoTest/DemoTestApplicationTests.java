829cd1f628aa9cbc850066c715c1efc1a49454c1
pilot <pilot77777@inbox.ru>
2023-05-11 22:21:04 +0300

    test123

---
+++ src/test/java/ru/alt/pop/demoTest/DemoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alt.pop.demoTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
